82bf78002014bf21ac2f0ec8264492428ae6af51
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-01-16 04:48:23 +0000

    [finish]システム開発について学ぼうまで

--- stdin_sample/readlines_practice.rb
+++ stdin_sample/readlines_practice.rb
@@ -1,5 +1,21 @@
+# lines = readlines
+# lines.each do |words|
+#   p words.chomp.split(' ')
+# end
+
+# 演習1
 lines = readlines
 lines.each do |words|
-  p words.chomp.split(' ')
+  word_h = words.chomp.split(' ')
+  word_h.each do |word|
+    p word
+  end
 end
 
+# 演習1回答
+# lines = readlines
+# lines.each do |words|
+#   words.chomp.split(' ').each do |word|
+#     p word
+#   end
+# end
